4cfe616a29bb6d8c12fffc7f3a5252effd91a692
Asmi Naithani <157035344+asminaithani@users.noreply.github.com>
2025-07-12 19:05:23 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";  // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not browser
+// ecma to read all the standards and documentations
+
+let name = "asmi"
+let age = 20
+let isLoggedIn = false
+
+//number => 2^53
+//bigint
+//string => ""
+//boolean => true/false
+//null => standalone value
+//undefined => value is not assigned
+//symbol => components, unique
+//object
+
+console.log(typeof null);
+console.log(typeof undefined);
+
