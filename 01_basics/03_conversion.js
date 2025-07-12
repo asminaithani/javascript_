bab298a2d71fcb1e6c1cd6987dd60468e478a339
Asmi Naithani <157035344+asminaithani@users.noreply.github.com>
2025-07-12 19:14:42 +0000

    conversions

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,13 @@
+let score = "33ab"
+console.log(typeof score);
+
+let valueInNum = Number(score)
+console.log(typeof valueInNum);
+console.log(valueInNum);
+
+//"33" => 33
+//"33ab" => NaN
+// true => 1
+// 1 => true
+//""=> false
+//"asmi"=> true
