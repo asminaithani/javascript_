f59948da21c75d17c7d24f4c2392be9be4f55db1
Asmi Naithani <157035344+asminaithani@users.noreply.github.com>
2025-07-13 11:53:20 +0000

    singleton objects

---
+++ 01_basics/05_objects.js
@@ -0,0 +1,47 @@
+//const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "222a"
+tinderUser.name = "sam"
+tinderUser.isLoggedIn = false
+
+//console.log(tinderUser);
+const regularUser = {
+    email: "some@gmail.com",
+    fullname: {
+        userfullname: {
+            firstname: "asmi",
+            lastname: "naithani"
+        }
+    }
+}
+//console.log(regularUser.fullname);
+//console.log(regularUser.fullname.userfullname.firstname);
+
+const obj1 = {1: "a", 2: "b"}
+const obj2 = {3: "a", 4: "b"}
+//const obj3 = {obj1, obj2}
+//const obj3 = Object.assign({}, obj1, obj2)
+const obj3 = {...obj1, ...obj2}
+
+//console.log(obj3);
+const users = [
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    },
+    {
+        id: 1,
+        email: "h@gmail.com"
+    }
+]
+users[1].email
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.values(tinderUser));
+console.log(tinderUser.hasOwnProperty(`isLoggedIn`));
+
