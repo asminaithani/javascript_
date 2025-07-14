2e6a7a520c97e4e9f8d845120cdcbebc11981ef7
Asmi Naithani <157035344+asminaithani@users.noreply.github.com>
2025-07-14 19:30:02 +0000

    loops

---
+++ 02_control/02_loops.js
@@ -0,0 +1,26 @@
+for (let i = 0; i < array.length; i++) {
+    const element = i;
+    if(element == 5){
+        console.log("5 is the best");
+    }
+    console.log(element);
+}
+
+let i = 0
+while(2==2){
+    console.log(`val of index is ${1}`);
+    i = i+2
+}
+
+let myArray = ['flash', "batman", "superman"]
+let arr = 0
+while(arr<myArray.length){
+    console.log(`value is ${myArray[arr]}`)
+    arr = arr+1
+}
+
+let score = 1
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <=10 );
