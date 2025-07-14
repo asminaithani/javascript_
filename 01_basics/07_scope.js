b8a28aec0487e219081fa460e9a80595f4463721
Asmi Naithani <157035344+asminaithani@users.noreply.github.com>
2025-07-14 12:58:44 +0000

    mini hoisting and scope level

---
+++ 01_basics/07_scope.js
@@ -0,0 +1,31 @@
+function one(){
+    const username = "asmi"
+
+    function two(){
+        const website = "youtube"
+        //console.log(username);
+    }
+    two()
+}
+one()
+
+if (true){
+    const username = "asmi"
+    if(username ==="asmi"){
+        const website = "youtube"
+        //console.log(username + website);
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+const addtwo = function(num){
+    return num + 2
+}
+console.log(addtwo(4))
